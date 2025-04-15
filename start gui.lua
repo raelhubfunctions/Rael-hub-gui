5487a74a74f0367b7cbfc6a6fd5a335da34abcdb
Rael Hub <israelbiscoitinho@gmail.com>
2025-04-15 13:17:28 -0300

    update start gui.lua

--- start gui.lua
+++
@@ -1,8 +0,0 @@
-local GuiModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/raelhubfunctions/rael-hub-gui/refs/heads/main/Module.lua"))()
-
-local ExecutorSupport = GuiModule.RunInterface()
-
-if ExecutorSupport then
-  GuiModule.setValueBar({Text = "Finishing"}
-  , 1)
-end
