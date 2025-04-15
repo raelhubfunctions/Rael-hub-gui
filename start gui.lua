c3fa4223ac62aaa860916bb76f259694b6fb117d
Rael Hub <israelbiscoitinho@gmail.com>
2025-04-15 10:46:13 -0300

    update

--- start gui.lua
+++ start gui.lua
@@ -5,13 +5,21 @@ local nameExecutor, versionExecutor = identifyexecutor()
 
 getgenv().RaelHubGuiLoad = ScreenGui
 
-local function setValueBar(valueText, valueSize)
-  if not valueText then return end
+local function setValueBar(valueTable, valueSize)
+  
+  if not valueTable or type(valueTable) ~= "table" then return end
+  
+  local valueText = valueTable.Text or "???"
+  local valueColor = valueTable.Color or Color3.fromRGB(17, 236, 139)
+  
   textInfo.Text = valueText
+  textInfo.TextColor3 = valueColor
+  
   if not valueSize then return end
+  
   containerBar.Size = UDim2.new(valueSize, 0, containerBar.Size.Y.Scale, 0)
 end
 
 task.wait(1)
 
-setValueBar(nameExecutor, 0.5)
+setValueBar({Text = nameExecutor}, 0.5)
